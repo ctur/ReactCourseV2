8951056f61365162f5365480b040c29275dd8e39
ctur <cturesoy@yahoo.com.tr>
2017-09-09 23:05:08 +0300

    Add files via upload

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,55 @@
-"use strict";
-
-console.log("app.js running!");
+'use strict';
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+); // -> Wrapper div!
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' Ctur'
+    ),
+    React.createElement(
+        'p',
+        null,
+        '  Age: 23'
+    ),
+    React.createElement(
+        'p',
+        null,
+        '  Location: Istanbul'
+    )
 );
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-console.log("app.js running!");
-
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+               <div>
+                 <h1>Indecision App</h1>
+                 <p>This is some info</p>
+                 <ol>
+                  <li>Item one</li>
+                  <li>Item two</li>
+                 </ol>
+               </div>
+); // -> Wrapper div!
+
+var templateTwo = (
+    <div>
+        <h1> Ctur</h1>
+        <p>  Age: 23</p>
+        <p>  Location: Istanbul</p>
+    </div>    
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 
