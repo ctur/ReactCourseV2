d35ec88659f5b0fbff33668872353ce3989da6d0
ctur <cturesoy@yahoo.com.tr>
2017-09-09 22:18:50 +0300

    Add files via upload

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,16 @@
+console.log("app.js running!");
+
+
+// JSX - JavaScript XML
+//var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+    "p",
+    null,
+    "This is JSX from app.js"
+  );
+  
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
